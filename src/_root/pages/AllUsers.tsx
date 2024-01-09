83dfdbab94df40851fd5d5a7c8a4732b0c3050d4
tish978 <satish.bisa@gmail.com>
2024-01-09 10:18:40 -0800

    second commit

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -5,35 +5,34 @@ import { Loader } from 'lucide-react'
 import React from 'react'
 
 const AllUsers = () => {
+  const { toast } = useToast();
 
-  const { toast } = useToast()
+  const { data: creators, isLoading, isError: isErrorCreators } = useGetUsers();
 
-  const { data: creators, isPending, isError: isErrorCreators } = useGetUsers()
-
-  if(isErrorCreators){
-    return toast({ title: 'Something went wrong.'})
+  if (isErrorCreators) {
+    toast({ title: "Something went wrong." });
+    
+    return;
   }
 
   return (
-    <div className='common-container'>
-      <div className='user-container'>
-        <h2 className='h3-bold md:h2-bold text-left w-full'>
-          All Users
-        </h2>
-        {isPending && !creators ? (
+    <div className="common-container">
+      <div className="user-container">
+        <h2 className="h3-bold md:h2-bold text-left w-full">All Users</h2>
+        {isLoading && !creators ? (
           <Loader />
         ) : (
-          <ul className='user-grid'>
+          <ul className="user-grid">
             {creators?.documents.map((creator) => (
-              <li>
-                <UserCard user={creator}/>
+              <li key={creator?.$id} className="flex-1 min-w-[200px] w-full  ">
+                <UserCard user={creator} />
               </li>
-            ))}  
+            ))}
           </ul>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AllUsers
+export default AllUsers;
